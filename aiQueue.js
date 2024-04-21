67db4e94dd55c88ba7d3608881a3c9855f64ab7f
Hugo H <huangfeiyang2008@gmail.com>
2024-04-20 18:47:01 -0700

    Co-authored-by: datboi212 <datboi212github@users.noreply.github.com>

--- aiQueue.js
+++ aiQueue.js
@@ -1,10 +1,8 @@
-import Queue from './queue.js';
-
 require('dotenv').config();
 const OpenAI = require('openai');
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-class Queue {
+export default class Queue {
 
   /** The constructor for the queue
    * elements: list of the elements inside the queue
@@ -65,18 +63,14 @@ class Queue {
     return false;
   }
 
+  processQueue() {
+    const completion = openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo-0125",
+    });
+  
+    console.log(completion.choices[0].message.content);
+  }
+
 }
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo-0125",
-  });
-
-  console.log(completion.choices[0].message.content);
-}
-
-main();
-
-const q = new Queue();
-console.log(q.getQueue());
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import InputParser from './inputParser.js';
+import Queue from './aiQueue.js';
 const inputParser = new InputParser();
-
+const queue = new Queue();
 inputParser.listen();
 setTimeout(() => {
   inputParser.stop();
